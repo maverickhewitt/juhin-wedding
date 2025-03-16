50fd97c28556efb5e8f04f5ce3c669a51d0e7cc4
Maverick Hewitt <mhewuniverse@gmail.com>
2025-03-16 14:30:56 +0800

    added slideshow function

--- script.js
+++ script.js
@@ -111,3 +111,23 @@ console.log(
   'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
 )
 
+let images = ['img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg','img/img6.jpg','img/img7.jpg','img/img8.jpg','img/img9.jpg','img/img10.jpg','img/img11.jpg','img/img12.jpg','img/img7b.jpg'];
+        let currentIndex = 0;
+
+        function changeImage(image) {
+            document.getElementById('mainImage').style.backgroundImage = `url(${image})`;
+            currentIndex = images.indexOf(image);
+        }
+
+        function nextImage() {
+            currentIndex = (currentIndex + 1) % images.length;
+            changeImage(images[currentIndex]);
+        }
+
+        function prevImage() {
+            currentIndex = (currentIndex - 1 + images.length) % images.length;
+            changeImage(images[currentIndex]);
+        }
+
+        // Initialize with the first image
+        changeImage(images[0]);
